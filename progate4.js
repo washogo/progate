b6d182eedd444dc759194d55e86c2b2a85966e4d
washogo <aiz4338150@gmail.com>
2021-10-19 02:37:40 +0900

    [Add]クラス内でのメソッドの定義とクラス内外での使用

--- progate4.js
+++ progate4.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+class Human{
+  constructor(weight,height){
+    this.weight=weight;
+    this.height=height;
+  }
+
+  introduction1(){
+    console.log(`体重は${this.weight}kgです`);
+  }
+
+  introduction2(){
+    console.log(`身長は${this.height}cmです`);
+    this.introduction1();
+  }
+}
+
+const man = new Human();
+man.introduction2();
